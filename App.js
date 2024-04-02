c6624a8f858245c24e47fc78145a2094d9daf14d
Vinaysankeerth13 <vinaysankeerth1@gmail.com>
2024-04-02 14:45:50 +0530

    End of Igniting out app

--- App.js
+++ App.js
@@ -1,17 +1,19 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
 const heading1 = React.createElement(
     "div",
     {id: "parent"},
     React.createElement(
       "div",
       {id: "child1"},
-      React.createElement("h1", {}, "I am h1 tag of child 1."),
-      React.createElement("h2", {}, "I'm a h2 tag of child 1")
+      React.createElement("h1", {}, "Vinay Sankeerth"),
+      React.createElement("h2", {}, "Sankeerth")
     ),
     React.createElement(
       "div",
       {id: "child2"},
-      React.createElement("h1", {}, "I am h1 tag of child 2"),
-      React.createElement("h2", {}, "I'm a h2 tag of child 2")
+      React.createElement("h1", {}, "Aadya"),
+      React.createElement("h2", {}, "Dodda")
     )
   );
   
